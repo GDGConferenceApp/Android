bfef35e2f55c9a84377292c49de919d85b9e36ee
fuentespatrick <fuentespatrick@gmail.com>
2015-12-02 17:26:33 -0600

    Stub Speaker adapter and model object

---
+++ app/src/main/java/mn/devfest/speakers/Speaker.java
@@ -0,0 +1,8 @@
+package mn.devfest.speakers;
+
+/**
+ * @author Patrick Fuentes <pfuentes@nerdery.com>
+ */
+public class Speaker {
+    //TODO implement
+}
---
+++ app/src/main/java/mn/devfest/speakers/SpeakerListAdapter.java
@@ -0,0 +1,47 @@
+package mn.devfest.speakers;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * @author Patrick Fuentes <pfuentes@nerdery.com>
+ */
+public class SpeakerListAdapter extends RecyclerView.Adapter<SpeakerListAdapter.SpeakerViewHolder> {
+
+    ArrayList<Speaker> mSpeakers;
+
+    @Override
+    public SpeakerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //TODO implement
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SpeakerViewHolder holder, int position) {
+        //TODO implement
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSpeakers == null ? 0 : mSpeakers.size();
+    }
+
+    public ArrayList<Speaker> getSpeakers() {
+        return mSpeakers;
+    }
+
+    public void setSpeakers(ArrayList<Speaker> speakers) {
+        mSpeakers = speakers;
+    }
+
+    public class SpeakerViewHolder extends RecyclerView.ViewHolder{
+        //TODO implement
+        public SpeakerViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
