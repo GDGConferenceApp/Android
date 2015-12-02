02cdd266adbfa4547a88aac3a2df3e5af4a49d44
fuentespatrick <fuentespatrick@gmail.com>
2015-12-01 19:22:12 -0600

    Add files missed in last commit

--- app/src/main/java/mn/devfest/sessions/SessionsFragment.java
+++ app/src/main/java/mn/devfest/sessions/SessionsFragment.java
@@ -4,23 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import mn.devfest.R;
 
 /**
  * Fragment that displays the available sessions
  *
  * @author bherbst
+ * @author pfuentes
  */
 public class SessionsFragment extends Fragment {
+
+    @Bind(R.id.session_list_recyclerview)
+    RecyclerView mSessionRecyclerView;
+
+    
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sessions, container, false);
+        View view = inflater.inflate(R.layout.fragment_sessions, container, false);
+        ButterKnife.bind(this, view);
+
+
+        return view;
     }
 
     @Override
