a0bcaa5c82ae9e4238e9574a4c5b4cfc1c2d4e3b
fuentespatrick <fuentespatrick@gmail.com>
2015-12-20 17:35:54 -0600

    Change name of placeholder text to repurpose it for more use Extract bundle contents in the fragment

--- app/src/main/java/mn/devfest/sessions/SessionDetailsFragment.java
+++ app/src/main/java/mn/devfest/sessions/SessionDetailsFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
 import mn.devfest.R;
 
 /**
@@ -18,9 +19,11 @@ import mn.devfest.R;
 public class SessionDetailsFragment extends Fragment {
     private static final String ARG_SESSION_ID = "sessionId";
 
-    public static SessionDetailsFragment newInstance(int speakerId) {
+    private Session mSession;
+
+    public static SessionDetailsFragment newInstance(int sessionId) {
         Bundle args = new Bundle();
-        args.putInt(ARG_SESSION_ID, speakerId);
+        args.putInt(ARG_SESSION_ID, sessionId);
 
         SessionDetailsFragment frag = new SessionDetailsFragment();
         frag.setArguments(args);
@@ -32,5 +35,26 @@ public class SessionDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_session_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        Bundle args = getArguments();
+        if (args != null && args.containsKey(ARG_SESSION_ID)) {
+            int sessionId = args.getInt(ARG_SESSION_ID);
+
+            // TODO get real session
+            mSession = new Session();
+            mSession.setId(sessionId);
+            mSession.setTitle("Session " + sessionId);
+        } else {
+            throw new IllegalStateException("SessionDetailsFragment requires a session ID passed via newInstance()");
+        }
+
+        //TODO Bind to the session
+        getActivity().setTitle(mSession.getTitle());
     }
 }
