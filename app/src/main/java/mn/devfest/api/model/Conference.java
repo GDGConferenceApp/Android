445e098fc5902b84fb4b48748d75651ec34d20b0
fuentespatrick <fuentespatrick@gmail.com>
2015-12-31 18:16:51 -0600

    Register a type adapter for Gson deserialization of JodaTime objects

--- app/src/main/java/mn/devfest/api/model/Conference.java
+++ app/src/main/java/mn/devfest/api/model/Conference.java
@@ -4,9 +4,15 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 
+import org.joda.time.DateTime;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
@@ -24,7 +30,13 @@ public class Conference {
     }
 
     public void parseSessions(JsonObject object) {
-        Gson gson = new GsonBuilder()
+        Gson gson = new GsonBuilder().
+                registerTypeAdapter(DateTime.class, new JsonDeserializer<DateTime>() {
+                    @Override
+                    public DateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                        return new DateTime(json.getAsString());
+                    }
+                })
                 .setDateFormat("yyyy-MM-dd'T'HH:mm")
                 .create();
         Set<Map.Entry<String,JsonElement>> entrySet = object.entrySet();
