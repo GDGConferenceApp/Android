07e523ed46e66e2fc6ff537b00b3bee33ff2c4de
Bryan Herbst <tanis.7x@gmail.com>
2016-01-19 18:43:22 -0600

    Alphabetized the speaker list

--- app/src/main/java/mn/devfest/speakers/SpeakerListFragment.java
+++ app/src/main/java/mn/devfest/speakers/SpeakerListFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,10 @@ import mn.devfest.api.DevFestDataSource;
 import mn.devfest.api.model.Session;
 import mn.devfest.api.model.Speaker;
 import mn.devfest.view.decoration.DividerItemDecoration;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Fragment that displays the list of speakers
@@ -33,16 +38,27 @@ public class SpeakerListFragment extends Fragment implements DevFestDataSource.D
     @Bind(R.id.speaker_list_recyclerview)
     RecyclerView mSpeakerRecyclerView;
 
+    @Bind(R.id.loading_progress)
+    ProgressBar mLoadingView;
+
     private SpeakerListAdapter mAdapter;
     private LinearLayoutManager mLinearLayoutManager;
 
     private List<Speaker> mSpeakerData = new ArrayList<>();
     private DevFestDataSource mDataSource;
+    private Subscription mDataUpdateSubscription;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mDataSource = DevFestApplication.get(getActivity()).component().datasource();
+        mDataSource.setDataSourceListener(this);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_speaker_list, container, false);
     }
 
@@ -60,11 +76,25 @@ public class SpeakerListFragment extends Fragment implements DevFestDataSource.D
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        mDataSource = DevFestApplication.get(getActivity()).component().datasource();
-        mDataSource.setDataSourceListener(this);
-        mSpeakerData = mDataSource.getSpeakers();
+    public void onResume() {
+        super.onResume();
+        //Refresh the UI with the latest data
+        List<Speaker> speakers = mDataSource.getSpeakers();
+
+        if (speakers.size() == 0) {
+            mLoadingView.setVisibility(View.VISIBLE);
+        } else {
+            setSpeakers(speakers);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mDataUpdateSubscription != null) {
+            mDataUpdateSubscription.unsubscribe();
+        }
     }
 
     /**
@@ -73,7 +103,32 @@ public class SpeakerListFragment extends Fragment implements DevFestDataSource.D
     @Override
     public void onDetach() {
         super.onDetach();
+        ButterKnife.unbind(this);
         //TODO cleanup resources
+    }
+
+    /**
+     * Notify this fragment that we have a new list of speakers for this conference
+     *
+     * @param speakers the new speakers
+     */
+    public void setSpeakers(List<Speaker> speakers) {
+        mDataUpdateSubscription = Observable.from(speakers)
+                .toSortedList((speaker1, speaker2) -> {
+                    return speaker1.getName().compareTo(speaker2.getName());
+                })
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::updateDisplayedSpeakers);
+    }
+
+    /**
+     * Update the list of which speakers are displayed
+     * @param speakers The speakers to display
+     */
+    private void updateDisplayedSpeakers(List<Speaker> speakers) {
+        mAdapter.setSpeakers(speakers);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -83,7 +138,8 @@ public class SpeakerListFragment extends Fragment implements DevFestDataSource.D
 
     @Override
     public void onSpeakersUpdate(List<Speaker> speakers) {
-        //TODO address update
+        setSpeakers(speakers);
+        mLoadingView.setVisibility(View.GONE);
     }
 
     @Override
