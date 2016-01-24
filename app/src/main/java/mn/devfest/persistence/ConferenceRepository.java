122481b91c77acdce708c36307b79a5fcb03be43
fuentespatrick <fuentespatrick@gmail.com>
2016-01-24 13:20:03 -0600

    Document methods in conference repository

--- app/src/main/java/mn/devfest/persistence/ConferenceRepository.java
+++ app/src/main/java/mn/devfest/persistence/ConferenceRepository.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import mn.devfest.api.model.Conference;
 
@@ -18,15 +19,26 @@ public class ConferenceRepository {
     Context mContext;
     SharedPreferences mSharedPreferences;
 
-    public ConferenceRepository (@NonNull Context context){
+    public ConferenceRepository(@NonNull Context context) {
         mContext = context;
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
     }
 
-    public void setConference(Conference conference) {
+    /**
+     * Removes any existing persisted conference data and replaces it with the passed value
+     *
+     * @param conference most up-to-date conference information available
+     */
+    public void setConference(@Nullable Conference conference) {
         //TODO implement
     }
 
+    /**
+     * Provides the most recently persisted conference data
+     *
+     * @return persisted conference data
+     */
+    @NonNull
     public Conference getConference() {
         //TODO implement and remove dummy return
         return new Conference();
