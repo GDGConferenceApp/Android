dae388eeaa068f55115dbd441e62046da355c8ff
fuentespatrick <fuentespatrick@gmail.com>
2017-01-08 17:11:46 -0600

    Retrieve list of speakers from Firebase

--- app/src/main/java/mn/devfest/speakers/SpeakerListFragment.java
+++ app/src/main/java/mn/devfest/speakers/SpeakerListFragment.java
@@ -5,10 +5,17 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +29,10 @@ import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import timber.log.Timber;
+
+import static mn.devfest.sessions.SessionsFragment.DEVFEST_2017_KEY;
+import static mn.devfest.sessions.SessionsFragment.SPEAKERS_CHILD_KEY;
 
 /**
  * Fragment that displays the list of speakers
@@ -39,8 +50,9 @@ public class SpeakerListFragment extends Fragment {
 
     private SpeakerListAdapter mAdapter;
 
-    private List<Speaker> mSpeakerData = new ArrayList<>();
+    private List<Speaker> mSpeakerList = new ArrayList<>();
     private Subscription mDataUpdateSubscription;
+    private DatabaseReference mFirebaseDatabaseReference;
 
 
     @Nullable
@@ -57,11 +69,31 @@ public class SpeakerListFragment extends Fragment {
         getActivity().setTitle(getResources().getString(R.string.speaker_title));
 
         mAdapter = new SpeakerListAdapter();
-        mAdapter.setSpeakers(mSpeakerData);
+        mAdapter.setSpeakers(mSpeakerList);
         mSpeakerRecyclerView.setAdapter(mAdapter);
         LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(getActivity());
         mSpeakerRecyclerView.setLayoutManager(mLinearLayoutManager);
         mSpeakerRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST));
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mFirebaseDatabaseReference.child(DEVFEST_2017_KEY).child(SPEAKERS_CHILD_KEY)
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Timber.d("Snapshot is: " + snapshot.toString());
+                    Speaker speaker = snapshot.getValue(Speaker.class);
+                    speaker.setId(snapshot.getKey());
+                    mSpeakerList.add(speaker);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(this.getClass().getSimpleName(), "Failed to read speakers value.", databaseError.toException());
+            }
+        });
     }
 
     @Override
