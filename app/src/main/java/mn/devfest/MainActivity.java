1c916dbcffd9bc1ba44dacf4bf8d27824296fd54
fuentespatrick <fuentespatrick@gmail.com>
2017-01-22 18:05:30 -0600

    Add firebase dependency Add authentication listener to main activity

--- app/src/main/java/mn/devfest/MainActivity.java
+++ app/src/main/java/mn/devfest/MainActivity.java
@@ -1,22 +1,32 @@
 package mn.devfest;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import mn.devfest.base.BaseActivity;
 import mn.devfest.map.MapFragment;
 import mn.devfest.schedule.UserScheduleFragment;
 import mn.devfest.sessions.SessionsFragment;
 import mn.devfest.speakers.SpeakerListFragment;
+import timber.log.Timber;
 
 /**
  * Main DevFest Activity. Handle navigation between top-level screens
  */
 public class MainActivity extends BaseActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     /**
      * Intent extra specifying a navigation destination
-     *
+     * <p>
      * The value associated with this extra should be the ID of the selected navigation item
      */
     public static final String EXTRA_NAVIGATION_DESTINATION = "navigation_destination";
@@ -30,6 +40,35 @@ public class MainActivity extends BaseActivity {
             int navId = getIntent().getIntExtra(EXTRA_NAVIGATION_DESTINATION, R.id.nav_schedule);
             navigateToTopLevelFragment(navId, false);
         }
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //User is signed in
+                    Timber.d("onAuthStateChanged() with user signed in as %s", user.getDisplayName());
+                } else {
+                    //User is signed out
+                    Timber.d("onAuthStateChanged() with user signed out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
@@ -39,9 +78,11 @@ public class MainActivity extends BaseActivity {
 
 
     //TODO evaluate if we want this public, or if we should try something else like listeners
+
     /**
      * Navigate to a top-level Fragment
-     * @param navItemId The ID of the top-level Fragment in the nav drawer
+     *
+     * @param navItemId      The ID of the top-level Fragment in the nav drawer
      * @param addToBackStack True to add this transition to the back stack
      */
     public void navigateToTopLevelFragment(int navItemId, boolean addToBackStack) {
@@ -67,7 +108,7 @@ public class MainActivity extends BaseActivity {
                 throw new IllegalArgumentException("Unknown item ID " + navItemId);
         }
 
-        FragmentTransaction transaction= getSupportFragmentManager()
+        FragmentTransaction transaction = getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.root_container, destination);
 
