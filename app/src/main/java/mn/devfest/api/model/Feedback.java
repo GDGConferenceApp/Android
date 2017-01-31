9a0974ff0484b8544514852ad0314008fb3cc3a5
Bryan Herbst <bryan.c.herbst@gmail.com>
2017-01-31 10:39:32 -0600

    Updated feedback model to reflect latest structure

--- app/src/main/java/mn/devfest/api/model/Feedback.java
+++ app/src/main/java/mn/devfest/api/model/Feedback.java
@@ -6,20 +6,26 @@ package mn.devfest.api.model;
  * @author bherbst
  */
 public class Feedback {
-    private final String sessionId;
-    private final String installId;
-    private final int overall;
-    private final int relevancy;
-    private final int difficulty;
-    private final int speakerQuality;
+    private final int speaker;
+    private final int content;
+    private final int recommendation;
 
 
-    public Feedback(String sessionId, String installId, int overall, int relevancy, int difficulty, int speakerQuality) {
-        this.sessionId = sessionId;
-        this.installId = installId;
-        this.overall = overall;
-        this.relevancy = relevancy;
-        this.difficulty = difficulty;
-        this.speakerQuality = speakerQuality;
+    public Feedback(int speaker, int content, int recommendation) {
+        this.speaker = speaker;
+        this.content = content;
+        this.recommendation = recommendation;
+    }
+
+    public int getSpeaker() {
+        return speaker;
+    }
+
+    public int getContent() {
+        return content;
+    }
+
+    public int getRecommendation() {
+        return recommendation;
     }
 }
